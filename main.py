a42007d09f937c0866af09ed3db6b4127a014e3a
btgoo <btgonbayr@gmail.com>
2023-10-18 01:35:18 +0100

    Third Commit

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+name = input("What's your name?")
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print("How are you?")
+print("Hi", name)

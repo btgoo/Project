220faa405af46810ac9a2e4a71fc0c39d1cce49d
btgoo <btgonbayr@gmail.com>
2023-10-27 15:29:58 +0100

    1st

---
+++ 10.27.py
@@ -0,0 +1,20 @@
+import random
+
+num_duplications = 0
+for day in range(10000):
+    birthdays = []
+
+    i = 1
+    while i <= 23:
+        days = random.randint(1, 365)
+        i += 1
+        birthdays.append(days)
+
+    noduplication = set(birthdays)
+
+    if len(noduplication) != len(birthdays):
+        num_duplications += 1
+
+probability = (num_duplications / 10000) * 100
+
+print(f"{probability:.2f}%")
